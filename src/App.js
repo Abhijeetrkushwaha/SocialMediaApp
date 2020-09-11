f0b9216b33f76de2e98f91fc7d1469edd1d80466
Abhijeetrkushwaha <abhijeetrkushwaha28@gmail.com>
2020-09-11 17:21:13 +0530

    added firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Nav from './components/Nav';
 import Post from './components/Post';
+import db from './firebase'
 
 function App() {
   const [posts, setPost] = useState([
@@ -9,9 +10,16 @@ function App() {
     {username: 'Jeet', projectName: 'image hub', id: 2, description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus, recusandae.'},
     {username: 'Kushwaha', projectName: 'git seacrh', id: 3, description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus, recusandae.'},
   ])
+  useEffect(() => {
+    db.collection('posts')
+    .onSnapshot(snapshot =>{
+        console.log(snapshot.docs.map(doc => doc.data()));
+    })
+  }, [] )
   return (
     <div className="app">
       <Nav />
+      <h1>abh</h1>
       <div className="dashboard">
         {
           posts.map(post => {
