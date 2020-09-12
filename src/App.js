bf799f67ac797097a7c4de0415fd55aef8c89474
Abhijeetrkushwaha <abhijeetrkushwaha28@gmail.com>
2020-09-12 18:19:45 +0530

    added dummy text

--- src/App.js
+++ src/App.js
@@ -2,28 +2,93 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Nav from './components/Nav';
 import Post from './components/Post';
-// import { db } from './firebase'
+import { db } from './firebase';
+import { makeStyles } from '@material-ui/core/styles';
+import { Modal, Button, Input } from '@material-ui/core';
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function App() {
-  const [posts, setPost] = useState([
-    {username: 'Abhijeet', projectName: 'chatapp', id: 1, description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus, recusandae.'},
-    {username: 'Jeet', projectName: 'image hub', id: 2, description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus, recusandae.'},
-    {username: 'Kushwaha', projectName: 'git seacrh', id: 3, description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus, recusandae.'},
-  ])
+  const classes = useStyles();
+  const [posts, setPosts] = useState([]);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [open, setOpen] = useState(false);
+  const [modalStyle] = useState(getModalStyle);
+
+  
   // useEffect(() => {
   //   db.collection('posts')
   //   .onSnapshot(snapshot =>{
-  //       console.log(snapshot.docs.map(doc => doc.data()));
+  //       setPosts(snapshot.docs.map(doc => (
+  //         {post: doc.data(), id: doc.id}
+  //       )))
   //   })
   // }, [] )
+  const signUp = (e) => {
+    e.preventDefault()
+    console.log(username, password, email);
+    setOpen(false)
+  }
+  
   return (
     <div className="app">
       <Nav />
-      <h1>abh</h1>
+      <Button onClick={() => setOpen(true)}>Sign up</Button>
+      <Modal
+        open={open}
+        onClose={() => setOpen(false)} 
+      >
+        <div style={modalStyle} className={classes.paper}>
+          <form>
+            <Input 
+              type="text"
+              placeholder="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <Input 
+              type="email"
+              placeholder="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input 
+              type="password"
+              placeholder="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <br/>
+            <Button onClick={signUp}>Submit</Button>
+          </form>
+        </div>
+      </Modal>
       <div className="dashboard">
         {
-          posts.map(post => {
-            return <Post username={post.username} projectName={post.projectName} description={post.description} key={post.id} />
+          posts.map(({post, id}) => {
+            return <Post username={post.username} projectName={post.projectName} description={post.description} key={id} />
           })
         }
       </div>
