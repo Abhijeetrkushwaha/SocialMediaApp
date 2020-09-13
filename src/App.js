6066b78639bf804fd41d3286d3ab70d003056557
Abhijeetrkushwaha <abhijeetrkushwaha28@gmail.com>
2020-09-13 13:07:39 +0530

    added dummy data

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Nav from './components/Nav';
 import Post from './components/Post';
-import { db } from './firebase';
+import { db, auth } from './firebase';
 import { makeStyles } from '@material-ui/core/styles';
 import { Modal, Button, Input } from '@material-ui/core';
 
@@ -35,7 +35,28 @@ function App() {
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
   const [open, setOpen] = useState(false);
+  const [openSignIn, setOpenSignIn] = useState(false);
   const [modalStyle] = useState(getModalStyle);
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        // user is logged in...
+        console.log(authUser);
+        setUser(authUser);
+
+      } else {
+        // user is logged out...
+        setUser(null)
+      }
+    })
+
+    return () => {
+      // perform some cleanup activity
+      unsubscribe()
+    }
+  }, [user, username]);
 
   
   // useEffect(() => {
@@ -48,14 +69,40 @@ function App() {
   // }, [] )
   const signUp = (e) => {
     e.preventDefault()
-    console.log(username, password, email);
+    
+    auth.createUserWithEmailAndPassword(email, password)
+    .then((authUser) => {
+      return authUser.user.updateProfile({
+        displayName: username,
+      })
+    })
+    .catch((err) => alert(err.message))
     setOpen(false)
+  }
+
+  const signIn = (e) => {
+    e.preventDefault()
+
+    auth.signInWithEmailAndPassword(email, password)
+    .catch((err) => alert(err.message))
+
+    setOpenSignIn(false)
   }
   
   return (
     <div className="app">
       <Nav />
-      <Button onClick={() => setOpen(true)}>Sign up</Button>
+      {
+        user ? (
+          <Button onClick={() => auth.signOut()}>Logout</Button>
+        ) : (
+          <div>
+            <Button onClick={() => setOpenSignIn(true)}>Login</Button>
+            <Button onClick={() => setOpen(true)}>Sign up</Button>
+          </div>
+        )
+      }
+      
       <Modal
         open={open}
         onClose={() => setOpen(false)} 
@@ -82,6 +129,31 @@ function App() {
             />
             <br/>
             <Button onClick={signUp}>Submit</Button>
+          </form>
+        </div>
+      </Modal>
+
+      {/* second modal */}
+      <Modal
+        open={openSignIn}
+        onClose={() => setOpenSignIn(false)} 
+      >
+        <div style={modalStyle} className={classes.paper}>
+          <form>
+            <Input 
+              type="email"
+              placeholder="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input 
+              type="password"
+              placeholder="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <br/>
+            <Button onClick={signIn}>Submit</Button>
           </form>
         </div>
       </Modal>
